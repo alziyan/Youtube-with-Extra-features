7a7ee2c410c9eb962cd732cb2226c886c1fd9e90
alziyan <ansari.alziyan@gmail.com>
2024-07-01 23:16:36 +0530

    search caching

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyCA3g0CrmPXtWjYER4L3jg4GUb5-R1P7lo";
 
 export const Youtube_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
